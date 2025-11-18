28b81493a9baf451674c288b359c6613af426376
daniel KAKUN <daniel.k1050233@gmail.com>
2025-11-18 11:08:01 +0200

    mm

---
+++ danielproject/JavaScript.js
@@ -0,0 +1,10 @@
+﻿
+var name = prompt("Enter your name:");
+document.write("Hello, " + name);
+var age = parseInt(prompt("Enter your age:"));
+document.write("next year you'll be:" + (age + 1))
+var num1 = parseInt(prompt("enter first num:"))
+var num2 = parseInt(prompt("enter second num:"))
+document.write("plus:" + (num1 * num2);
+document.write("minus:" + (num1 - num2));
+document.write("kefel:" + (num1 * num2));
